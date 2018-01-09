2e9a0b88404ae44a2094b6b266f8569a080ab2d1
Jonas Natten <jonas.natten@knowit.no>
2018-01-09 09:54:43 +0100

    #841: Fixes bug where matched language only returned if matched on last field in search

--- src/main/scala/no/ndla/draftapi/service/search/ConceptSearchService.scala
+++ src/main/scala/no/ndla/draftapi/service/search/ConceptSearchService.scala
@@ -57,13 +57,12 @@ trait ConceptSearchService {
       val contentSearch = simpleStringQuery(query).field(s"content.$language", 1)
 
       val hi = highlight("*").preTag("").postTag("").numberOfFragments(0)
-      val ih = innerHits("inner_hits").highlighting(hi)
 
       val fullQuery = boolQuery()
         .must(boolQuery()
           .should(
-            nestedQuery("title", titleSearch).scoreMode(ScoreMode.Avg).boost(2).inner(ih),
-            nestedQuery("content", contentSearch).scoreMode(ScoreMode.Avg).boost(1).inner(ih)
+            nestedQuery("title", titleSearch).scoreMode(ScoreMode.Avg).boost(2).inner(innerHits("title").highlighting(hi)),
+            nestedQuery("content", contentSearch).scoreMode(ScoreMode.Avg).boost(1).inner(innerHits("content").highlighting(hi))
           )
         )
 
