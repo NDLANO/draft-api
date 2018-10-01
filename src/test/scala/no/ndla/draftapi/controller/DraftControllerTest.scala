d12876edb72c2be98aabefcd348b6199e87957e1
Jonas Natten <jonas.natten@knowit.no>
2018-10-01 15:39:12 +0200

    #1293: Adds import_validation to handle tags

--- src/test/scala/no/ndla/draftapi/controller/DraftControllerTest.scala
+++ src/test/scala/no/ndla/draftapi/controller/DraftControllerTest.scala
@@ -198,7 +198,7 @@ class DraftControllerTest extends UnitSuite with TestEnvironment with ScalatraFu
   }
 
   test("PUT /:id/validate/ should return 204 if user has required permissions") {
-    when(contentValidator.validateArticleApiArticle(any[Long])).thenReturn(Success(ContentId(1)))
+    when(contentValidator.validateArticleApiArticle(any[Long], any[Boolean])).thenReturn(Success(ContentId(1)))
     put("/test/1/validate/") {
       status should equal(200)
     }
