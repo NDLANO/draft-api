42f3b3bf52f22f35cf3611aa5f88e85240217387
Jonas Natten <jonas.natten@knowit.no>
2018-11-16 15:50:21 +0100

    WIP Adds failing test for updating article

--- src/test/scala/no/ndla/draftapi/integration/ArticleApiClientTest.scala
+++ src/test/scala/no/ndla/draftapi/integration/ArticleApiClientTest.scala
@@ -7,7 +7,8 @@
 
 package no.ndla.draftapi.integration
 
-import no.ndla.draftapi.model.api.ContentId
+import no.ndla.draftapi.model.api.{ContentId, Copyright}
+import no.ndla.draftapi.model.{api, domain}
 import no.ndla.draftapi.{TestData, TestEnvironment, UnitSuite}
 import no.ndla.network.AuthUser
 import org.json4s.DefaultFormats
@@ -26,6 +27,10 @@ class ArticleApiClientTest extends UnitSuite with TestEnvironment {
 
   val idResponse = ContentId(1)
 
+  val exampleToken =
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik9FSTFNVVU0T0RrNU56TTVNekkyTXpaRE9EazFOMFl3UXpkRE1EUXlPRFZDUXpRM1FUSTBNQSJ9.eyJodHRwczovL25kbGEubm8vY2xpZW50X2lkIjogInh4eHl5eSIsICJpc3MiOiAiaHR0cHM6Ly9uZGxhLmV1LmF1dGgwLmNvbS8iLCAic3ViIjogInh4eHl5eUBjbGllbnRzIiwgImF1ZCI6ICJuZGxhX3N5c3RlbSIsICJpYXQiOiAxNTEwMzA1NzczLCAiZXhwIjogMTUxMDM5MjE3MywgInNjb3BlIjogImFydGljbGVzLXRlc3Q6cHVibGlzaCBkcmFmdHMtdGVzdDp3cml0ZSBkcmFmdHMtdGVzdDpzZXRfdG9fcHVibGlzaCBhcnRpY2xlcy10ZXN0OndyaXRlIiwgImd0eSI6ICJjbGllbnQtY3JlZGVudGlhbHMifQ.gsM-U84ykgaxMSbL55w6UYIIQUouPIB6YOmJuj1KhLFnrYctu5vwYBo80zyr1je9kO_6L-rI7SUnrHVao9DFBZJmfFfeojTxIT3CE58hoCdxZQZdPUGePjQzROWRWeDfG96iqhRcepjbVF9pMhKp6FNqEVOxkX00RZg9vFT8iMM"
+  val authHeaderMap = Map("Authorization" -> exampleToken)
+
   test("allocating an article and concept id should return a long") {
     forgePact
       .between("draft-api")
@@ -36,7 +41,7 @@ class ArticleApiClientTest extends UnitSuite with TestEnvironment {
           .uponReceiving(method = POST,
                          path = "/intern/id/article/allocate",
                          query = None,
-                         headers = Map("Authorization" -> TestData.authHeaderWithWriteRole),
+                         headers = authHeaderMap,
                          body = None,
                          matchingRules = None)
           .willRespondWith(
@@ -52,7 +57,7 @@ class ArticleApiClientTest extends UnitSuite with TestEnvironment {
           .uponReceiving(method = POST,
                          path = "/intern/id/concept/allocate",
                          query = None,
-                         headers = Map("Authorization" -> TestData.authHeaderWithWriteRole),
+                         headers = authHeaderMap,
                          body = None,
                          matchingRules = None)
           .willRespondWith(
@@ -63,10 +68,29 @@ class ArticleApiClientTest extends UnitSuite with TestEnvironment {
           )
       )
       .runConsumerTest { mockConfig =>
-        AuthUser.setHeader(TestData.authHeaderWithWriteRole)
+        AuthUser.setHeader(exampleToken)
         val articleApiClient = new ArticleApiClient(mockConfig.baseUrl)
         articleApiClient.allocateArticleId(List("1234", "4567"), List("1", "2")) should be(Success(1))
         articleApiClient.allocateConceptId(List("1234", "4567")) should be(Success(1))
+      }
+  }
+
+  test("updating articles and concepts should work and return updated version") {
+    val articleToUpdate = TestData.sampleDomainArticle
+
+    forgePact
+      .between("draft-api")
+      .and("article-api")
+      .addInteraction(
+        interaction
+          .description("Updating an article returns the same article")
+          .uponReceiving(POST, "/intern/article/1", None, authHeaderMap, write(articleToUpdate), None)
+          .willRespondWith(200)
+      )
+      .runConsumerTest { mockConfig =>
+        AuthUser.setHeader(exampleToken)
+        val articleApiClient = new ArticleApiClient(mockConfig.baseUrl)
+        articleApiClient.updateArticle(1, articleToUpdate, List("1234"))
       }
   }
 }
