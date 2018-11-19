29e875c61b3589fcdb8c4da68faba2616af1469f
Jonas Natten <jonas.natten@knowit.no>
2018-11-19 07:35:07 +0100

    Adds consumer test for updating article

--- src/test/scala/no/ndla/draftapi/integration/ArticleApiClientTest.scala
+++ src/test/scala/no/ndla/draftapi/integration/ArticleApiClientTest.scala
@@ -7,11 +7,14 @@
 
 package no.ndla.draftapi.integration
 
+import java.util.Date
+
 import no.ndla.draftapi.model.api.{ContentId, Copyright}
+import no.ndla.draftapi.model.domain.Status
 import no.ndla.draftapi.model.{api, domain}
 import no.ndla.draftapi.{TestData, TestEnvironment, UnitSuite}
 import no.ndla.network.AuthUser
-import org.json4s.DefaultFormats
+import org.json4s.{DefaultFormats, Formats}
 import org.json4s.native.Serialization.write
 
 import scala.util.Success
@@ -76,14 +79,44 @@ class ArticleApiClientTest extends UnitSuite with TestEnvironment {
   }
 
   test("updating articles and concepts should work and return updated version") {
-    val articleToUpdate = TestData.sampleDomainArticle
+    implicit val formats: Formats = domain.Article.formats
+    val copyright = domain.Copyright(
+      Some("CC-BY-SA-4.0"),
+      Some("Origin"),
+      Seq.empty,
+      Seq.empty,
+      Seq.empty,
+      None,
+      None,
+      None
+    )
+
+    val articleToUpdate = domain.Article(
+      id = Some(1),
+      revision = Some(1),
+      status = domain.Status(domain.ArticleStatus.PUBLISHED, Set.empty),
+      title = Seq(domain.ArticleTitle("Title", "nb")),
+      content = Seq(domain.ArticleContent("Content", "nb")),
+      copyright = Some(copyright),
+      tags = Seq(domain.ArticleTag(List("Tag1", "Tag2", "Tag3"), "nb")),
+      requiredLibraries = Seq(),
+      visualElement = Seq(),
+      introduction = Seq(),
+      metaDescription = Seq(domain.ArticleMetaDescription("Meta Description", "nb")),
+      metaImage = Seq(),
+      created = new Date(0),
+      updated = new Date(0),
+      updatedBy = "updatedBy",
+      articleType = domain.ArticleType.Standard,
+      notes = Seq()
+    )
 
     forgePact
       .between("draft-api")
       .and("article-api")
       .addInteraction(
         interaction
-          .description("Updating an article returns the same article")
+          .description("Updating an article returns 200")
           .uponReceiving(POST, "/intern/article/1", None, authHeaderMap, write(articleToUpdate), None)
           .willRespondWith(200)
       )
