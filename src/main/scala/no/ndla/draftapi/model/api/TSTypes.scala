b0b01e279e2a8b6fc73058d9f92e202f3e1759ff
Jonas Natten <jonas.natten@knowit.no>
2021-10-11 14:42:40 +0200

    Fix  type generation

--- src/main/scala/no/ndla/draftapi/model/api/TSTypes.scala
+++ src/main/scala/no/ndla/draftapi/model/api/TSTypes.scala
@@ -1,13 +1,20 @@
 package no.ndla.draftapi.model.api
 
+import com.scalatsi.TypescriptType.TSNull
 import com.scalatsi._
 import com.scalatsi.dsl._
 
+/**
+  * The `scala-tsi` plugin is not always able to derive the types that are used in `Seq` or other generic types.
+  * Therefore we need to explicitly load the case classes here.
+  * This is only necessary if the `sbt generateTypescript` script fails.
+  */
 object TSTypes {
   implicit val author = TSType.fromCaseClass[Author]
   implicit val requiredLibrary = TSType.fromCaseClass[RequiredLibrary]
   implicit val editorNote = TSType.fromCaseClass[EditorNote]
   implicit val relatedContentLink = TSType.fromCaseClass[RelatedContentLink]
-
-  implicit val x = TSType.fromCaseClass[NewArticleMetaImage]
+  implicit val newArticleMetaImage = TSType.fromCaseClass[NewArticleMetaImage]
+  // This alias is required since scala-tsi doesn't understand that Null is `null`
+  implicit val nullAlias = TSType.alias[Null]("NullAlias", TSNull)
 }
